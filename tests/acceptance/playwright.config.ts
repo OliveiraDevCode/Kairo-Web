8b5f33c94167d8432b6c74d47e968d4a04de427d
Ricardo Oliveira <oliveiradevcode@gmail.com>
2026-06-17 09:00:52 -1200

    test: fix playwright test discovery

--- tests/acceptance/playwright.config.ts
+++ tests/acceptance/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests/acceptance",
+  testDir: "./features",
 
   testMatch: "**/*.spec.ts",
 
